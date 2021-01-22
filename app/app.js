464ae9a050834ca7938c6a982518910866aee834
Junya-0220 <junya010220@gmail.com>
2021-01-22 18:45:21 +0900

    全てコピーした

--- app/app.js
+++ app/app.js
@@ -6,11 +6,11 @@ const bodyParser = require('body-parser')
 
 const dbPath = "app/db/database.sqlite3"
 
-//リクエストのbodyをパースする設定
+// リクエストのbodyをパースする設定
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-//publicディレクトリを静的ファイル群のルートディレクトリとして設定
+// publicディレクトリを静的ファイル群のルートディレクトリとして設定
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Get all users
@@ -32,7 +32,28 @@ app.get('/api/v1/users/:id', (req, res) => {
   const id = req.params.id
 
   db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
-    res.json(row)
+    if (!row) {
+      res.status(404).send({error: "Not Found!"})
+    } else {
+      res.status(200).json(row)
+    }
+  })
+
+  db.close()
+})
+
+// Get following users
+app.get('/api/v1/users/:id/following', (req, res) => {
+  // Connect database
+  const db = new sqlite3.Database(dbPath)
+  const id = req.params.id
+
+  db.all(`SELECT * FROM following LEFT JOIN users ON following.followed_id = users.id WHERE following_id = ${id};`, (err, rows) => {
+    if (!rows) {
+      res.status(404).send({error: "Not Found!"})
+    } else {
+      res.status(200).json(rows)
+    }
   })
 
   db.close()
@@ -51,78 +72,102 @@ app.get('/api/v1/search', (req, res) => {
   db.close()
 })
 
-//Create a new user
-
-app.post('/api/v1/users',async (req,res) => {
-  const db = new sqlite3.Database(dbPath)
-
-  const name = req.body.name
-  const profile = req.body.profile ? req.body.profile : ""
-  const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
-
-  await run(`INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}","${dateOfBirth}")`,
-  db,
-  res,
-  "ユーザー情報を更新しました！"
-  )
-  db.close()
-})
-
-const run = async(sql, db, res, message ) => {
+const run = async (sql, db) => {
   return new Promise((resolve, reject) => {
-    db.run(sql,(err) => {
-      if(err) {
-        res.status(500).send(err)
-        return reject()
-      }else{
-        res.json({message: message})
+    db.run(sql, (err) => {
+      if (err) {
+        return reject(err)
+      } else {
         return resolve()
       }
     })
   })
 }
 
-//Update a user data
+// Create a new user
+app.post('/api/v1/users', async (req, res) => {
+  if (!req.body.name || req.body.name === "") {
+    res.status(400).send({error: "ユーザー名が指定されていません。"})
+  } else {
+    // Connect database
+    const db = new sqlite3.Database(dbPath)
 
+    const name = req.body.name
+    const profile = req.body.profile ? req.body.profile : ""
+    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
+
+    try {
+      await run(
+        `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`,
+        db
+      )
+      res.status(201).send({message: "新規ユーザーを作成しました。"})
+    } catch (e) {
+      res.status(500).send({error: e})
+    }
+
+    db.close()
+  }
+})
+
+// Update user data
 app.put('/api/v1/users/:id', async (req, res) => {
+  if (!req.body.name || req.body.name === "") {
+    res.status(400).send({error: "ユーザー名が指定されていません。"})
+  } else {
+    // Connect database
+    const db = new sqlite3.Database(dbPath)
+    const id = req.params.id
+
+    // 現在のユーザー情報を取得する
+    db.get(`SELECT * FROM users WHERE id=${id}`, async (err, row) => {
+
+      if (!row) {
+        res.status(404).send({error: "指定されたユーザーが見つかりません。"})
+      } else {
+        const name = req.body.name ? req.body.name : row.name
+        const profile = req.body.profile ? req.body.profile : row.profile
+        const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
+
+        try {
+          await run(
+            `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
+            db
+          )
+          res.status(200).send({message: "ユーザー情報を更新しました。"})
+        } catch (e) {
+          res.status(500).send({error: e})
+        }
+      }
+    })
+
+    db.close()
+  }
+})
+
+// Delete user data
+app.delete('/api/v1/users/:id', async (req, res) => {
   // Connect database
   const db = new sqlite3.Database(dbPath)
   const id = req.params.id
 
   // 現在のユーザー情報を取得する
   db.get(`SELECT * FROM users WHERE id=${id}`, async (err, row) => {
-    const name = req.body.name ? req.body.name : row.name
-    const profile = req.body.profile ? req.body.profile : row.profile
-    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
 
-    await run(
-      `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
-      db,
-      res,
-      "ユーザー情報を更新しました！"
-    )
+    if (!row) {
+      res.status(404).send({error: "指定されたユーザーが見つかりません。"})
+    } else {
+      try {
+        await run(`DELETE FROM users WHERE id=${id}`, db)
+        res.status(200).send({message: "ユーザーを削除しました。"})
+      } catch (e) {
+        res.status(500).send({error: e})
+      }
+    }
   })
 
   db.close()
 })
-
-  
-//Delete a user data
-app.delete('/api/v1/users/:id', async (req, res) => {
-  // Connect database
-  const db = new sqlite3.Database(dbPath)
-  const id = req.params.id
-
-  await run(
-    `DELETE FROM users WHERE id=${id}`,
-    db,
-    res,
-    "ユーザー情報を削除しました！"
-  )
-
-  db.close()
-})
-
 
 const port = process.env.PORT || 3000;
 app.listen(port)
--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,45 +1,41 @@
 const indexModule = (() => {
-    const path = window.location.pathname
+  const path = window.location.pathname
 
-    switch(path) {
-        case '/':
-            // 検索ボタンをクリックした時のイベントリスナー設定
-            document.getElementById('search-btn').addEventListener('click', () => {
-              return searchModule.searchUsers()
-            })
-          
-            // UsersモジュールのfetchAllUsersメソッドを呼び出す
-            return usersModule.fetchAllUsers()
+  switch (path) {
+    case '/':
+      // 検索ボタンをクリックした時のイベントリスナー設定
+      document.getElementById('search-btn').addEventListener('click', () => {
+        return searchModule.searchUsers()
+      })
 
-        case '/create.html':
-            // 保存するをクリックした時のイベントリスナー設定
-            document.getElementById('save-btn').addEventListener('click', () => {
-            return usersModule.createUsers()
-            })
-            // キャンセルをクリックした時のイベントリスナー設定
-            document.getElementById('cancel-btn').addEventListener('click', () => {
-            return window.location.href = "/"
-            })
-            break;
+      // UsersモジュールのfetchAllUsersメソッドを呼び出す
+      return usersModule.fetchAllUsers()
 
-        case '/edit.html':
-          const uid = window.location.search.split("?uid=")[1]
-            // 保存するをクリックした時のイベントリスナー設定
-            document.getElementById('save-btn').addEventListener('click', () => {
-            return usersModule.saveUsers(uid)
-            })
-            // キャンセルをクリックした時のイベントリスナー設定
-            document.getElementById('cancel-btn').addEventListener('click', () => {
-            return window.location.href = "/"
-            })
-            //削除するボタンを押したとき
-            document.getElementById('delete-btn').addEventListener('click', () => {
-              return usersModule.deleteUser(uid)
-              })
+    case '/create.html':
+      document.getElementById('save-btn').addEventListener('click', () => {
+        return usersModule.createUser()
+      })
+      document.getElementById('cancel-btn').addEventListener('click', () => {
+        return window.location.href = '/'
+      })
+      break;
 
-              return usersModule.setExisitingValue(uid)
-        
-            default:
-            break;
-    }
-  })()
+    case '/edit.html':
+      const uid = window.location.search.split('?uid=')[1]
+
+      document.getElementById('save-btn').addEventListener('click', () => {
+        return usersModule.saveUser(uid)
+      })
+      document.getElementById('cancel-btn').addEventListener('click', () => {
+        return window.location.href = '/'
+      })
+      document.getElementById('delete-btn').addEventListener('click', () => {
+        return usersModule.deleteUser(uid)
+      })
+
+      return usersModule.setExistingValue(uid)
+
+    default:
+      break;
+  }
+})()
